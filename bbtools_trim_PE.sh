3e9b75789c1e0a6824ae9cd98ab7626d079218af
Erin Roberts <erin_roberts@my.uri.edu>
2017-07-18 11:05:59 -0400

    added bbtools script and undated paired end read targets

--- bbtools_trim_PE.sh
+++ bbtools_trim_PE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS-l nodes=1
+#PBS-l nodes=2
 #PBS-l walltime=1000:00:00
 #PBS -j oe
 
@@ -11,7 +11,7 @@ echo "START $(date)"
 module load BBMap/37.36-foss-2016b-Java-1.8.0_131
 
 #all files are in the home directory and either have ending 
-# _1.fastq or _2.fastq
+# _1.fq or _2.fq
 #going to make two array variables and then iterate through them as an index
 #changing all the file endings to .fq to see if bbduk.sh prefers that file name ending
 
@@ -121,12 +121,40 @@ array2=(
    SRR526975_2.fq
 )
  
-array3=(
-)
 for index in ${!array[*]}; do    
-   bbduk.sh in1=${array[$index]} in2=${array2[$index]} k=23 ref=adapters.fa stats=stats${array[$index]}.txt out=stdout${array[$index]}.fq
+   bbduk.sh in1=${array[$index]} in2=${array2[$index]} k=23 ref=adapters.fa stats=stats${array[$index]}.txt out=stdout${array[$index]}
 done
 
-# stats.txt will then list the names of adapter sequences found, and their frequency
+  # stats.txt will then list the names of adapter sequences found, and their frequency
+
+#need to find out if the adaptor sequences are on the left side or the right side
+
+# Trimming of adaptors found in the previous command
+# bbduk.sh in1=${array[$index]} out1=clean_${array[$index]} in2=${array2[$index]} out2=clean_${array2[$index]} ref=adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo
+	
+	#ktrim = r means it will only trim from right side, while ktrim=l only trims from left
+	#hdist = hamming distance, hdist =1 allows for 1 mismatch
+	#flag -tbo specifies to also trim adaptors based on pir overlap detection using BBMerge 
+	#which does not require known adapter sequences)
+	#flag -tpe specified to trim both reads to the same length (if the adapter kmer was only detected in one of them and not other)
+
+
+#quality trimming, of both the left and the right sides to get rid of reads that are less than quality 10
+	
+# for index in ${!array[*]}; do
+	#bbduk.sh in1=clean_${array[$index]} out1=quality_trim_${array[$index]} in2=clean_${array2[$index]} out2=quality_trim_${array2[$index]}  qtrim=rl trimq=10
+# done
+
+
+#quality filtering to get rid of entire low quality reads. maq=10 will trim reads that have average quality of less than 10
+ #for index in ${!array[*]}; do
+	#bbduk.sh in1=quality_trim_${array[$index]} out1=quality_trim_and_filtered_${array[$index]} in2=quality_trim_${array2[$index]} out2=quality_trim_and_filtered_${array2[$index]}  maq=10
+ #done
+
+#histogram generation
+# for index in ${!array[*]}; do
+	#bbduk.sh in1=quality_trim_and_filtered_${array[$index]} in2=quality_trim_and_filtered_${array2[$index]} bhist=bhist.txt qhist=qhist.txt gchist=gchist.txt aqhist=aqhist.txt lhist=lhist.txt gcbins=auto
+# done
+
 
 echo "STOP $(date)"
