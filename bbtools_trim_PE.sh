57af96753544ccab988ff408f5a9aa7f12454c89
Erin Roberts <erin_roberts@my.uri.edu>
2017-07-27 15:49:40 -0400

    committed exact scripts used on command line to run the separate trimming for SE and PE reads

--- bbtools_trim_PE.sh
+++ bbtools_trim_PE.sh
@@ -10,119 +10,19 @@ set -e
 echo "START $(date)"
 module load BBMap/37.36-foss-2016b-Java-1.8.0_131
 
+F=/data3/marine_diseases_lab/erin/Bio_project_SRA/PE_fastq
+
 #all files are in the home directory and either have ending 
 # _1.fq or _2.fq
 #going to make two array variables and then iterate through them as an index
 #changing all the file endings to .fq to see if bbduk.sh prefers that file name ending
 
-array=(
-   ERR498201_1.fq
-   SRR2601695_1.fq
-   SRR2601706_1.fq 
-   SRR334213_1.fq
-   SRR334339_1.fq
-   SRR623231_1.fq
-   SRR1060328_1.fq
-   SRR2601696_1.fq
-   SRR2601707_1.fq
-   SRR334214_1.fq
-   SRR334340_1.fq
-   SRR826503_1.fq
-   SRR2601482_1.fq
-   SRR2601698_1.fq 
-   SRR2601709_1.fq
-   SRR334215_1.fq
-   SRR497890_1.fq
-   SRR957603_1.fq
-   SRR2601571_1.fq 
-   SRR2601699_1.fq
-   SRR2601711_1.fq 
-   SRR334216_1.fq  
-   SRR497891_1.fq
-   SRR960386_1.fq
-   SRR2601666_1.fq
-   SRR2601701_1.fq
-   SRR2601712_1.fq
-   SRR334217_1.fq
-   SRR5043896_1.fq
-   SRR967068_1.fq
-   SRR2601689_1.fq  
-   SRR2601702_1.fq
-   SRR2601714_1.fq
-   SRR334218_1.fq 
-   SRR5043897_1.fq
-   SRR2601690_1.fq
-   SRR2601703_1.fq
-   SRR2601716_1.fq
-   SRR334219_1.fq
-   SRR5043898_1.fq
-   SRR2601692_1.fq
-   SRR2601704_1.fq
-   SRR2601718_1.fq
-   SRR334220_1.fq
-   SRR5043899_1.fq
-   SRR2601694_1.fq
-   SRR2601705_1.fq
-   SRR334212_1.fq
-   SRR334221_1.fq
-   SRR526975_1.fq
-)
+ array1=($(ls $F/*_1.fq))
+ array2=($(ls $F/*_2.fq))
 
-array2=(
-   ERR498201_2.fq   
-   SRR2601695_2.fq  
-   SRR2601706_2.fq  
-   SRR334213_2.fq  
-   SRR334339_2.fq   
-   SRR623231_2.fq
-   SRR1060328_2.fq  
-   SRR2601696_2.fq  
-   SRR2601707_2.fq  
-   SRR334214_2.fq  
-   SRR334340_2.fq  
-   SRR826503_2.fq
-   SRR2601482_2.fq 
-   SRR2601698_2.fq  
-   SRR2601709_2.fq  
-   SRR334215_2.fq  
-   SRR497890_2.fq   
-   SRR957603_2.fq
-   SRR2601571_2.fq  
-   SRR2601699_2.fq  
-   SRR2601711_2.fq  
-   SRR334216_2.fq  
-   SRR497891_2.fq   
-   SRR960386_2.fq
-   SRR2601666_2.fq  
-   SRR2601701_2.fq  
-   SRR2601712_2.fq  
-   SRR334217_2.fq  
-   SRR5043896_2.fq  
-   SRR967068_2.fq
-   SRR2601689_2.fq  
-   SRR2601702_2.fq  
-   SRR2601714_2.fq  
-   SRR334218_2.fq  
-   SRR5043897_2.fq
-   SRR2601690_2.fq  
-   SRR2601703_2.fq  
-   SRR2601716_2.fq  
-   SRR334219_2.fq  
-   SRR5043898_2.fq
-   SRR2601692_2.fq  
-   SRR2601704_2.fq  
-   SRR2601718_2.fq  
-   SRR334220_2.fq  
-   SRR5043899_2.fq
-   SRR2601694_2.fq  
-   SRR2601705_2.fq  
-   SRR334212_2.fq   
-   SRR334221_2.fq  
-   SRR526975_2.fq
-)
  
-for index in ${!array[*]}; do    
-   bbduk.sh in1=${array[$index]} in2=${array2[$index]} k=23 ref=adapters.fa stats=stats${array[$index]}.txt out=stdout${array[$index]}
+for i in ${array1[@]}; do  # @ symbol tells it to go through each item in the array  
+   bbduk.sh in1=${i} in2=$(echo ${i}|sed s/_1/_2/) k=23 ref=/opt/software/BBMap/37.36-foss-2016b-Java-1.8.0_131/resources/adapters.fa stats=${i}.stat out=${i}.out
 done
 
   # stats.txt will then list the names of adapter sequences found, and their frequency
@@ -130,9 +30,10 @@ done
 #need to find out if the adaptor sequences are on the left side or the right side
 
 # Trimming of adaptors found in the previous command
-# bbduk.sh in1=${array[$index]} out1=clean_${array[$index]} in2=${array2[$index]} out2=clean_${array2[$index]} ref=adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo
+
+# bbduk.sh in1=${i} out1=${i}.clean in2=$(echo ${i}|sed s/_1/_2/) out2=$(echo ${i}|sed s/_1/_2/).clean ref=adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo
 	
-	#ktrim = r means it will only trim from right side, while ktrim=l only trims from left
+	#ktrim = r means it will only trim from right side, which is where the adapter should be. (ktrim=l would trim from left)
 	#hdist = hamming distance, hdist =1 allows for 1 mismatch
 	#flag -tbo specifies to also trim adaptors based on pir overlap detection using BBMerge 
 	#which does not require known adapter sequences)
@@ -156,5 +57,10 @@ done
 	#bbduk.sh in1=quality_trim_and_filtered_${array[$index]} in2=quality_trim_and_filtered_${array2[$index]} bhist=bhist.txt qhist=qhist.txt gchist=gchist.txt aqhist=aqhist.txt lhist=lhist.txt gcbins=auto
 # done
 
+#tophat
+	#bowtie2-build
+#cufflinks
+
+#RSEM
 
 echo "STOP $(date)"
