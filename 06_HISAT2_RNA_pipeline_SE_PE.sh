e4a511ec768d33d042e3d5598e99288662877d42
Erin Roberts <erin_roberts@my.uri.edu>
2017-08-06 11:14:42 -0400

    updated HISAT2 script

--- 06_HISAT2_RNA_pipeline_SE_PE.sh
+++ 06_HISAT2_RNA_pipeline_SE_PE.sh
@@ -13,7 +13,7 @@ module load SAMtools/1.3.1-foss-2016b
 
 #HISAT2 code
 
-#Indexing a reference genome
+#Indexing a reference genome and no annotation file
 	#create new directory for the index called genome, and put the genome inside it
 
 cd /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_genome/genome/
@@ -22,25 +22,34 @@ hisat2-build -f /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_geno
 
 # -f indicates that the reference input files are FASTA files
 
+#Indexing a reference genome with an annotation file 
+
 #Stay in the directory created in the previous step
 
-#aligning paired end reads
+#Aligning paired end reads
 P=/data3/marine_diseases_lab/erin/Bio_project_SRA/PE_fastq/PE_revised
 
 for f in $P/*.fq.clean.trim.filter; do
-	hisat2 -x /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_genome/genome/genome_index -1 ${f}_1.fq.clean.trim.filter -2 ${f}_2.fq.clean.trim.filter -S {f}.sam
+	hisat2 --dta -x /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_genome/genome/genome_index -1 ${f}_1.fq.clean.trim.filter -2 ${f}_2.fq.clean.trim.filter -S {f}.sam
 done
  	#don't need -f because the reads are fastq
-
+	# put -x before the index
+	# --dta : Report alignments tailored for transcript assemblers including StringTie.
+	 #With this option, HISAT2 requires longer anchor lengths for de novo discovery of splice sites. 
+	 #This leads to fewer alignments with short-anchors, which helps transcript assemblers improve significantly in computation and memory usage.
 #Aligning single end reads
 S=/data3/marine_diseases_lab/erin/Bio_project_SRA/SAR
 
-hisat2 -x /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_genome/genome_index -U /data3/marine_diseases_lab/erin/Bio_project_SRA/reads_1.fa -S eg1.sam
+hisat2 --dta -x /data3/marine_diseases_lab/erin/Crassostrea_gigas_reference_genome/genome_index -U /data3/marine_diseases_lab/erin/Bio_project_SRA/reads_1.fa -S ${f}.sam
 
 	#put -x right before the genome index base, and the -U right before unpaired reads
+
 	
 This runs the HISAT2 aligner, which aligns a set of unpaired reads to the genome region using the index generated in the 
 
 
-#SAMTOOLS view to convert the SAM file into a BAM file to be used with StringTie
+#SAMTOOLS sort to convert the SAM file into a BAM file to be used with StringTie
+
+samtools sort -@ 8 -o ERR188044_chrX.bam ERR188044_chrX.sam $ samtools sort -@ 8 -o ERR188104_chrX.bam ERR188104_chrX.sam $ samtools sort -@ 8 -o ERR188234_chrX.bam ERR188234_chrX.sam $ samtools sort -@ 8 -o ERR188245_chrX.bam ERR188245_chrX.sam $ samtools sort -@ 8 -o ERR188257_chrX.bam ERR188257_chrX.sam $ samtools sort -@ 8 -o ERR188273_chrX.bam ERR188273_chrX.sam $ samtools sort -@ 8 -o ERR188337_chrX.bam ERR188337_chrX.sam $ samtools sort -@ 8 -o ERR188383_chrX.bam ERR188383_chrX.sam $ samtools sort -@ 8 -o ERR188401_chrX.bam ERR188401_chrX.sam $ samtools sort -@ 8 -o ERR188428_chrX.bam ERR188428_chrX.sam $ samtools sort -@ 8 -o ERR188454_chrX.bam ERR188454_chrX.sam $ samtools sort -@ 8 -o ERR204916_chrX.bam ERR204916_chrX.sam
+
 
