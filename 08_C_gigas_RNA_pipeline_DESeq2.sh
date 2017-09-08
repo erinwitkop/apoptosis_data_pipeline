fdbc910f34b8be05c7b3c4569fd5aad46af9f48b
Erin Roberts <erin_roberts@my.uri.edu>
2017-09-08 14:24:12 -0400

    deleted old script

--- 08_C_gigas_RNA_pipeline_DESeq2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#PBS-l nodes=3
-#PBS-l walltime=1000:00:00
-#PBS -j oe
-
-#8_2_17
-
-#Script to take Salmon output of read abundances and pipe those into DESeq2 for normalization and Differential Expression Analysis
