9a6d3aeddae25aa9c6e4d7b9ef01c50dadb77573
Erin Roberts <erin_roberts@my.uri.edu>
2017-09-26 13:37:51 -0400

    corrected get fasta script for significantly expressed MSTRG transcripts in OsHV1 and Bacterial challenge

--- SCRIPTS/Bac_Viral_Subset/06_MSTRG_isolate_getfasta.sh
+++ SCRIPTS/Bac_Viral_Subset/06_MSTRG_isolate_getfasta.sh
@@ -11,6 +11,7 @@ set -e
 echo "START" $(date)
 
 module load bio/BEDTools/2.26.0-foss-2016b
+F=/data3/marine_diseases_lab/erin/Bio_project_SRA/pipeline_files/Bac_Viral_subset
 
 #Using BEDtools getfasta in order to pull out MSTRG gene sequences after subsetting the stringtie.merge.gtf
 #file for the sequences that were significantly differentially expressed
@@ -18,11 +19,12 @@ module load bio/BEDTools/2.26.0-foss-2016b
 #Find line with Matching stringtie geneID found in the StringTie --merge output file
 #Make file for all the MSTRGID_Gene_Sig.txt with all the significantly differentially expressed MSTRG transcripts
 
-cut -f2 MSTRGID_Gene_Sig_oshv1 > MSTRGID_Gene_Sig_oshv1.txt
+#cut -f2 OsHv1_MSTRGID_tran_Sig > OsHv1_MSTRGID_tran_Sig.txt
+#cut -f2 Bac_MSTRGID_tran_Sig > Bac_MSTRGID_tran_Sig.txt
 
-# USAGE: bash 06_MSTRG_isolate_Sig_nonSig.sh > Bac_viral_MSTRG_GENE_SIG_merged.gtf
+# USAGE: bash 06_MSTRG_isolate_getfasta.sh > OsHv1_MSTRGID_tran_SIG_merged.gtf
 
-args=($(cat MSTRGID_Gene_Sig_oshv1.txt))
+args=($(cat $F/OsHv1_MSTRGID_tran_Sig.txt))
 while read -r line
 do
     for i in ${args[@]}
@@ -31,18 +33,31 @@ do
             *"$i"*) echo "$line";;
         esac
     done
-done <"Bac_viral_stringtie_merged.gtf"
+done <$F/"Bac_viral_stringtie_merged.gtf"
+
+#args=($(cat $F/Bac_MSTRGID_tran_Sig.txt))
+#while read -r line
+#do
+#    for i in ${args[@]}
+#    do
+#        case "$line" in
+#            *"$i"*) echo "$line";;
+#        esac
+#    done
+#done <$F/"Bac_viral_stringtie_merged.gtf"
+
 
 #isolate just the transcripts 
-awk '$3=="transcript"' Bac_viral_MSTRG_GENE_SIG_merged.gtf > Bac_viral_MSTRG_GENE_SIG_merged_noexons.gtf
+awk '$3=="transcript"' $F/OsHv1_MSTRGID_tran_SIG_merged.gtf > $F/OsHv1_MSTRGID_tran_SIG_merged_noexons.gtf
+
+# awk '$3=="transcript"' $F/Bac_MSTRGID_tran_SIG_merged.gtf > $F/Bac_MSTRGID_tran_SIG_merged_noexons.gtf
 
 #Use BEDTools to extract the sequences of MSTRG genes
 #getfasta will extract the sequence in the orientation defined in the strand column when the “-s” option is used.
 #tip: the headers in the input FASTA file must exactly match the chromosome column in the BED file.
 #test this script on a subset first to see if it works properly...
 
-
-bedtools getfasta -fi Crassostrea_gigas_genome.fa -bed Bac_viral_MSTRG_GENE_SIG_merged_noexons.gtf -fo OUTPUT_FASTA -s
+bedtools getfasta -fi $F/Crassostrea_gigas_genome.fa -bed $F/OsHv1_MSTRGID_tran_SIG_merged_noexons.gtf -fo $F/Bac_viral_MSTRG_merged_SEQUENCE.fa -s
 
 #IN ORDER TO GET FULL GENE LOCATIONS, YOU NEED TO HAVE GOTTEN THE FEATURE COUNTS SCRIPT OR GET THE -A GENE LOCATION OUTPUT
 
