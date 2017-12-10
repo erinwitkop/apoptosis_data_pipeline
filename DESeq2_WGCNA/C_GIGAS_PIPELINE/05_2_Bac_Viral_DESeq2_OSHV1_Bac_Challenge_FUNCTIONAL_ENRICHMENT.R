1eaf0601cb6d20061e4bca0daa3edc6f71afb785
Erin Roberts <erin_roberts@my.uri.edu>
2017-12-10 15:03:29 -0500

    fixed and redid gene enrichment analysis

--- DESeq2_WGCNA/C_GIGAS_PIPELINE/05_2_Bac_Viral_DESeq2_OSHV1_Bac_Challenge_FUNCTIONAL_ENRICHMENT.R
+++ DESeq2_WGCNA/C_GIGAS_PIPELINE/05_2_Bac_Viral_DESeq2_OSHV1_Bac_Challenge_FUNCTIONAL_ENRICHMENT.R
@@ -1,10 +1,11 @@
 #05_2_Bac_Viral_DESeq2_OSHV1_Bac_Challenge_FUNCTIONAL_ENRICHMENT.R
 
 #this script utilized topGO to perform Gene Set Ennrichment Analysis (GSEA)
+#downloaded the Generic GO slim from the GO consortium, http://www.geneontology.org/page/go-slim-and-subset-guide
 
 #Load packages
-#source("http://bioconductor.org/biocLite.R")
-#biocLite(c("topGO","ALL","Rgraphviz"))
+source("http://bioconductor.org/biocLite.R")
+biocLite(c("topGO","ALL","Rgraphviz"))
 library(topGO)
 #install.packages("tm")
 library(tm)
@@ -15,117 +16,30 @@ library(dplyr)
 library(plyr)
 library(genefilter)
 library(Rgraphviz)
+#see what ontologies are supported
 
-####BLAST2GO the MSTRG SIG genes (AND MSTRG NON SIG GENES), then perform gene set enrichment on these and pull out more specific apoptosis genes ####
-##subset out only the genes that have "MSTRG ID"
-#lookup these lines in the stringtie.merge file to find the sequence for them (SEE 06_MSTRG_isolate_getfasta.sh), then put them into BLAST2GO
-#Sig MSTRG's from OsHV1
-resoshv1Tran_05_dfSig_Transcript_MSTRG <- resoshv1Tran_05_dfSig[grep("MSTRG", rownames(resoshv1Tran_05_dfSig)), ] #for Significant genes
-OsHv1_MSTRGID_tran_Sig <- as.data.frame(rownames(resoshv1Tran_05_dfSig_Transcript_MSTRG))
-head(OsHv1_MSTRGID_tran_Sig)
-write.table(OsHv1_MSTRGID_tran_Sig[,1], file="OsHv1_MSTRGID_tran_Sig", sep= "\t")
+ls("package:GO.db")
 
-#NON SI MSTRGs OsHV1
-resoshv1Tran_05_df_non_Sig_Transcript_MSTRG <- resoshv1Tran_05_df_non_Sig[grep("MSTRG", rownames(resoshv1Tran_05_df_non_Sig)),]
-OsHV1_MSTRGID_tran_non_Sig <- as.data.frame(rownames(resoshv1Tran_05_df_non_Sig_Transcript_MSTRG))
-head(OsHV1_MSTRGID_tran_non_Sig)
-write.table(OsHV1_MSTRGID_tran_non_Sig[,1], file="OsHv1_MSTRGID_tran_non_Sig", sep= "\t")
-
-#Sig MSTRGs from Bacterial Challenge
-resBacTran_05_dfSig_Transcript_MSTRG <- resBacTran_05_dfSig[grep("MSTRG", rownames(resBacTran_05_dfSig)), ]
-Bac_MSTRGID_tran_Sig <- as.data.frame(rownames(resoshv1Tran_05_dfSig_Transcript_MSTRG))
-head(Bac_MSTRGID_tran_Sig)
-write.table(Bac_MSTRGID_tran_Sig[,1], file="Bac_MSTRGID_tran_Sig", sep= "\t")
-
-#NON SIG MSTRGs from Bacterial Challenge
-resBacTran_05_df_non_Sig_MSTRG <- resBacTran_05_df_non_Sig[grep("MSTRG", rownames(resBacTran_05_df_non_Sig)),]
-Bac_MSTRG_tran_non_Sig <- as.data.frame(rownames(resBacTran_05_df_non_Sig_MSTRG))
-head(Bac_MSTRG_tran_non_Sig)
-write.table(write.table(Bac_MSTRG_tran_non_Sig[,1], file="Bac_MSTRGID_tran_non_Sig", sep= "\t"))
-
-#USE 06_MSTRG_isolate_getfasta.sh script to get the sequence of genes while using the bam files of each and the sequence
-#INSERT SIG SEQUENCES SEQUENCES INTO BLAST2GO
-
-####If only using annotated genes!#####
-
+#Only using those genes that already have annotated terms
 
 ####Data preparation####
 #input is a table that contains the genes, the GO mappings, and the p-values
 #OsHV1
-head(resoshv1Tran_05_df)
-OsHV1_topGOsubset <- 
-  resoshv1Tran_05_df[grep("transcript:", rownames(resoshv1Tran_05_df)), ]
-head(OsHV1_topGOsubset)
-nrow(OsHV1_topGOsubset) #rows = 12112
-#make a new column with the rownames, called Ensembl_genomes_transcript
-OsHV1_topGOsubset$Ensembl_Genomes_Transcript<-rownames(OsHV1_topGOsubset)  
-##needs to be made a data frame so that can be joined later by plyr
-OsHV1_topGOsubset_df <- as.data.frame(OsHV1_topGOsubset)  
-OsHV1_topGOtranscript_subset <- as.data.frame(rownames(OsHV1_topGOsubset))
-head(OsHV1_topGOtranscript_subset)
-colnames(OsHV1_topGOtranscript_subset) <- "transcript"
-OsHV1transcriptIDdf <- OsHV1_topGOtranscript_subset %>%
-  separate(transcript, c("transcript", "EKC"), ":")
-#write to string
-OsHV1_topGO_transcriptIDstring <- toString(OsHV1transcriptIDdf[,2], sep=',')
-write(OsHV1_topGO_transcriptIDstring, "OsHV1_topGO_transcriptIDstring", sep = ",")
-#cut off commas with sed 's/,//g' INFILE > outfile
-
-#Load annotation file
-OsHV1_topGO_annotations <- read.csv("OsHV1_topGO_GOIDS.csv", sep=",", header=TRUE) #12092 rows
-nrow(OsHV1_topGO_annotations)
-head(OsHV1_topGO_annotations)
-#add "transcript: before every string" in Ensembl Genomes Transcript
-OsHV1_topGO_annotations$Ensembl_Genomes_Transcript = paste('transcript', OsHV1_topGO_annotations$Ensembl_Genomes_Transcript, sep=':')
-#concatentate gene expression and uniprot table
-#Perform join by plyr
-OsHV1_topGO_annotations
-OsHV1_topGO_annotations_FULL <- join(OsHV1_topGO_annotations, OsHV1_topGOsubset_df, by="Ensembl_Genomes_Transcript")
-nrow(OsHV1_topGO_annotations_FULL) #12092 This is what I wanted 
+head(resoshvTran_05_df_FULL)
+colnames(resoshvTran_05_df_FULL)
+oshv_GO_table <- resoshvTran_05_df_FULL[,c("ID","padj", "Protein.names", "Gene.ontology.IDs")]
+head(oshv_GO_table)
 
 #Bac
-head(resBacTran_05_df)
-BAC_topGOsubset <- 
-  resBacTran_05_df[grep("transcript:", rownames(resBacTran_05_df)), ]
-head(BAC_topGOsubset)
-#make a new column with the rownames, called Ensembl_genomes_transcript
-BAC_topGOsubset$Ensembl_Genomes_Transcript<-rownames(BAC_topGOsubset)  
-##needs to be made a data frame so that can be joined later by plyr
-BAC_topGOsubset_df <- as.data.frame(BAC_topGOsubset)  #use this for the plyr join!
-BAC_topGOtranscript_subset <- as.data.frame(rownames(BAC_topGOsubset))
-head(BAC_topGOtranscript_subset)
-colnames(BAC_topGOtranscript_subset) <- "transcript"
-BACtranscriptIDdf <- BAC_topGOtranscript_subset %>%
-  separate(transcript, c("transcript", "EKC"), ":") #edited to make rownames
-nrow(BACtranscriptIDdf) #8075
-#write to string so I can do lookup
-BAC_topGO_transcriptIDstring <- toString(BACtranscriptIDdf[,2], sep=',')
-write(BAC_topGO_transcriptIDstring, "BAC_topGO_transcriptIDstring", sep = ",")
-
-#Load annotation file
-BAC_topGO_annotations <- read.csv("Bac_topGO_transcript_GOIDs.csv", sep=",", header=TRUE) #include GO IDs
-#8065 out of 8075 were mapped to 8092
-nrow(BAC_topGO_annotations) #8092
-head(BAC_topGO_annotations)
-#add "transcript: before every string" in Ensembl Genomes Transcript
-BAC_topGO_annotations$Ensembl_Genomes_Transcript = paste('transcript', BAC_topGO_annotations$Ensembl_Genomes_Transcript, sep=':')
-#concatentate gene expression and uniprot table
-#Perform join by plyr
-BAC_topGO_annotations_FULL <- join(BAC_topGO_annotations, BAC_topGOsubset_df, by="Ensembl_Genomes_Transcript")
-nrow(BAC_topGO_annotations_FULL) #8092
-
+colnames(resBacTran_05_df_FULL)
+Bac_GO_table <- resBacTran_05_df_FULL[,c("ID","padj", "Protein.names", "Gene.ontology.IDs")]
+head(Bac_GO_table)
 
 #####Prepare topGO data ####
-#filtering step using genefilter?
 
-#subset for correct rows
-OsHV1_data <- OsHV1_topGO_annotations_FULL[,c("Ensembl_Genomes_Transcript", "Gene_names", "Protein_names","pvalue","padj", "Gene_ontology_IDs")]
-nrow(OsHV1_data) #12092
-Bac_data <- BAC_topGO_annotations_FULL[,c("Ensembl_Genomes_Transcript", "Gene_names", "Protein_names","pvalue","padj", "Gene_ontology_IDs")]
-nrow(Bac_data) #8092
 #remove genes with no mapping
-OsHV1_data_noblanks <- OsHV1_data[!(OsHV1_data$Gene_ontology_IDs == ""), ] #7958 rows
-Bac_data_noblanks <- Bac_data[!(Bac_data$Gene_ontology_IDs == ""),] #5518 rows
+OsHV1_data_noblanks <- oshv_GO_table[!(oshv_GO_table$ Gene.ontology.IDs == ""), ] #7958 rows
+Bac_data_noblanks <- Bac_GO_table[!(Bac_GO_table$ Gene.ontology.IDs == ""), ] #5518 rows
 
 #Create topGO object
 #This object will contain all information necessary for the GO analysis, 
@@ -138,32 +52,37 @@ Bac_data_noblanks <- Bac_data[!(Bac_data$Gene_ontology_IDs == ""),] #5518 rows
 #file format required by readMappings = gene_ID<TAB>GO_ID1, GO_ID2, GO_ID3,
 
 #format GO mapping files to readMapping format
-OsHV1_readMapping <- OsHV1_data_noblanks[,c("Ensembl_Genomes_Transcript", "Gene_ontology_IDs")]
-Bac_readMapping <- Bac_data_noblanks[,c("Ensembl_Genomes_Transcript", "Gene_ontology_IDs")]
+OsHV1_readMapping <- OsHV1_data_noblanks[,c("ID", "Gene.ontology.IDs")]
+Bac_readMapping <- Bac_data_noblanks[,c("ID", "Gene.ontology.IDs")]
 write.table(OsHV1_readMapping, file="OsHV1_readMapping", row.names=FALSE, sep= "\t")
 write.table(Bac_readMapping, file="Bac_readMapping", row.names=FALSE, sep= "\t") 
-    #must be tab delimited
+#must be tab delimited
 #reformat using bash commands (remove quotes, change semicolon to commas, remove "transcript:")
-# sed -e 's/\;/,/g' Bac_readMapping > Bac_readMapping_commas
-#sed 's/\"//g' Bac_readMapping_commas > Bac_readMapping_finished.txt
-# sed 's/\"//g' OsHv1_readMapping_commas > OsHV1_readMapping_finished.txt
-#sed 's/^.\{11\}//g' Bac_readMapping_finished.txt > Bac_readMapping_finished2.txt
-#remove first line: tail -n +2 Bac_readMapping_finished2.txt > Bac_readMapping_finished3.txt
+sed 's/\"//g' Bac_readMapping > Bac_readMapping_quotes.txt
+sed 's/\"//g' OsHV1_readMapping > OsHV1_readMapping_quotes.txt
+ sed -e 's/\;/,/g' Bac_readMapping_quotes.txt > Bac_readMapping_commas.txt
+sed -e 's/\;/,/g' OsHV1_readMapping_quotes.txt > OsHV1_readMapping_commas.txt
+tail -n +2 Bac_readMapping_commas.txt > Bac_readMapping_finished.txt
+tail -n +2 OsHV1_readMapping_commas.txt > OsHV1_readMapping_finished.txt
 
+  #sed 's/^.\{12\}//g' Bac_readMapping_finished.txt > Bac_readMapping_finished2.txt
+ # sed 's/^.\{12\}//g' OsHV1_readMapping_finished.txt > OsHV1_readMapping_finished2.txt
+ 
 #Use readMappings()
-OsHV1geneID2GO <- readMappings(file = "OsHV1_readMapping_finished3.txt")
+OsHV1geneID2GO <- readMappings(file = "OsHV1_readMapping_finished.txt")
 str(head(OsHV1geneID2GO))
-BacgeneID2GO <- readMappings(file= "Bac_readMapping_finished3.txt")
+BacgeneID2GO <- readMappings(file= "Bac_readMapping_finished.txt")
 str(head(BacgeneID2GO))
+
+#how to use a GO slim ontology with topGO
 
 #Defining your list of genes of interest, and the 'gene universe' to compare it to
 #Define genes of interest: the number of genes with a padj less than 0.05
 #OsHV1 
 OsHV1geneUniverse <- names(OsHV1geneID2GO) 
 OsHV1_topDiff <- OsHV1_data_noblanks[ which( OsHV1_data_noblanks$padj < 0.05 ), ]
+head(OsHV1_topDiff)
 OsHV1_geneofInterest <- OsHV1_topDiff[,1] #need to remove "transcript:
-stopwords="transcript:"
-OsHV1_geneofInterest <- removeWords(OsHV1_geneofInterest,stopwords)
 #tell TopGO where these interesting genes appear in the 'geneUniverse' vector:
 #geneList tells topGO which genes are of interest
 OsHV1geneList <- factor(as.integer(OsHV1geneUniverse %in% OsHV1_geneofInterest))
@@ -182,14 +101,12 @@ OsHV1_GOdata
 #the specified genes of interest are the ones called significant
 OsHV1_sg <- sigGenes(OsHV1_GOdata)
 str(OsHV1_sg)
-numSigGenes(OsHV1_GOdata) 
+numSigGenes(OsHV1_GOdata) #70
 
 #Bacterial challenge genes
 BacgeneUniverse <- names(BacgeneID2GO) 
 Bac_topDiff <- Bac_data_noblanks[which( Bac_data_noblanks$padj < 0.05), ]
-Bac_geneofInterest <- Bac_topDiff[,1] #need to remove "transcript:
-stopwords="transcript:"
-Bac_geneofInterest <- removeWords(Bac_geneofInterest,stopwords)
+Bac_geneofInterest <- Bac_topDiff[,1] 
 #tell TopGO where these interesting genes appear in the 'geneUniverse' vector:
 #geneList tells topGO which genes are of interest
 BacgeneList <- factor(as.integer(BacgeneUniverse %in% Bac_geneofInterest))
@@ -213,14 +130,14 @@ OsHV1FisherParentchild <- runTest(OsHV1_GOdata, algorithm="parentchild", statist
 # see how many results we get where weight01 gives a P-value <= 0.05:
 OsHV1summary <- summary(attributes(OsHV1Fisher_weight)$score <= 0.05)
 OsHV1_numsignif <- as.integer(OsHV1summary[[3]]) # how many terms is it true that P <= 0.05
-OsHV1_numsignif: 6!
+OsHV1_numsignif: 7
   
 #print out the top 'numsignif' results:
 OsHV1_numsignif_Res <- GenTable(OsHV1_GOdata, classicFisher = OsHV1Fisher,
         elimFisher = OsHV1FisherElim, topgoFisher = OsHV1Fisher_weight,
         parentchildFisher = OsHV1FisherParentchild, orderBy = "topgoFisher", 
         ranksOf = "classicFisher", topNodes = OsHV1_numsignif)
-write.csv(OsHV1_numsignif_Res, file = "OsHV1_numsignif_Res.csv")
+write.csv(OsHV1_numsignif_Res, file = "OsHV1_numsignif_Res_NEW.csv")
 
 # print a graph (to a pdf file) with the top 'numsignif' results from weight01 analysis:
 printGraph(OsHV1_GOdata, OsHV1Fisher_weight, firstSigNodes = OsHV1_numsignif, useInfo = "all", pdfSW = TRUE)
@@ -228,7 +145,7 @@ printGraph(OsHV1_GOdata, OsHV1Fisher_weight, firstSigNodes = OsHV1_numsignif, us
 
 #Find top significant nodes 
 OsHV1_topRes <- GenTable(OsHV1_GOdata, classicFisher = OsHV1Fisher, orderBy = "resultFisher", ranksOf = "classicFisher", topNodes = 20)
-write.csv(OsHV1_topRes, file= "OsHV1_topGO_topSignificantNodes.csv")
+write.csv(OsHV1_topRes, file= "OsHV1_topGO_topSignificantNodes_NEW.csv")
 
 # print out the genes that are annotated with the significantly enriched GO terms:
 OsHV1_terms <- OsHV1_numsignif_Res$GO.ID
@@ -242,17 +159,22 @@ for (i in 1:length(OsHV1_terms))
   OsHV1genesforterm2 <- paste(OsHV1genesforterm2, collapse=',')
   print(paste("Term",OsHV1_GOterm,"transcripts:",OsHV1genesforterm2))
 }
+#[1] "Term GO:0043248 transcripts: EKC34293,EKC42394"
+#[1] "Term GO:0006298 transcripts: EKC25057,EKC26394"
+#[1] "Term GO:0007154 transcripts: EKC17376,EKC28428,EKC33131,EKC35537"
+#[1] "Term GO:0015992 transcripts: EKC18664,EKC37019"
+#[1] "Term GO:0007010 transcripts: EKC17965,EKC18223,EKC21714,EKC35202,EKC35203"
+#[1] "Term GO:0045454 transcripts: EKC32832,EKC37672,EKC39509"
+#[1] "Term GO:0006040 transcripts: EKC27126,EKC40852"
+
 
 #Find gene name for these genes, listed in OsHV1_data
-#need to remove "transcript:", add back new column
-OsHV1_data_edited1 <- removeWords(OsHV1_data$Ensembl_Genomes_Transcript, stopwords)
-OsHV1_data_edited2<- cbind(OsHV1_data, OsHV1_data_edited1)
 #Lookup table with match(), return indices of match in second arg to first arg:
-OsHV1_sigGO_name_lookup <- read.csv("OsHV1_sigGO_transcriptIDs.csv", header=TRUE, sep=",")
-OsHV1_sigGO_gene_names_index <- OsHV1_data_edited2$OsHV1_data_edited1 %in% OsHV1_sigGO_name_lookup$OsHV1_data_edited1
-!is.na(match(OsHV1_data_edited2$OsHV1_data_edited1,OsHV1_sigGO_name_lookup$OsHV1_data_edited1))
-OsHV1_sigGO_gene_names <- OsHV1_data_edited2[OsHV1_sigGO_gene_names_index, ]
-write.csv(OsHV1_sigGO_gene_names, file="OsHV1_sigGO_gene_names.csv")
+OsHV1_sigGO_name_lookup <- read.csv("OsHV1_sigGO_transcriptIDs_NEW.csv", header=TRUE, sep=",")
+OsHV1_sigGO_gene_names_index <- oshv_GO_table$ID %in% OsHV1_sigGO_name_lookup$ID
+!is.na(match(oshv_GO_table$ID,oshv_GO_table$ID))
+OsHV1_sigGO_gene_names <- oshv_GO_table[OsHV1_sigGO_gene_names_index, ]
+write.csv(OsHV1_sigGO_gene_names, file="OsHV1_sigGO_gene_names_NEW.csv")
 
 #### Bac Enrichment Analysis ####
 #Fisher's Exact test
@@ -266,23 +188,22 @@ BacFisherParentchild <- runTest(Bac_GOdata, algorithm="parentchild", statistic="
 # see how many results we get where weight01 gives a P-value <= 0.05:
 Bacsummary <- summary(attributes(BacFisher_weight)$score <= 0.05)
 Bac_numsignif <- as.integer(Bacsummary[[3]]) # how many terms is it true that P <= 0.05
-Bac_numsignif # = 4 
+Bac_numsignif # = 2 
   
 #print out the top 'numsignif' results:
 Bac_numsignif_Res <- GenTable(Bac_GOdata, classicFisher = BacFisher,
                                   elimFisher = BacFisherElim, topgoFisher = BacFisher_weight,
                                   parentchildFisher = BacFisherParentchild, orderBy = "topgoFisher", 
                                   ranksOf = "classicFisher", topNodes = Bac_numsignif)
-write.csv(Bac_numsignif_Res, file= "Bac_numsignif_Res.csv")
+write.csv(Bac_numsignif_Res, file= "Bac_numsignif_Res_NEW.csv")
 
 # print a graph (to a pdf file) with the top 'numsignif' results from weight01 analysis:
 printGraph(Bac_GOdata, BacFisher_weight, firstSigNodes = Bac_numsignif, useInfo = "all", pdfSW = TRUE)
-#no. nodes = 38
 #rename your graph! 
 
 #Find top significant nodes 
 Bac_topRes <- GenTable(Bac_GOdata, classicFisher = BacFisher, orderBy = "resultFisher", ranksOf = "classicFisher", topNodes = 20)
-write.csv(Bac_topRes, file="Bac_topGO_topSignificantNodes.csv")
+write.csv(Bac_topRes, file="Bac_topGO_topSignificantNodes_NEW.csv")
 
 # print out the genes that are annotated with the significantly enriched GO terms:
 Bac_terms <- Bac_numsignif_Res$GO.ID
@@ -296,36 +217,35 @@ for (i in 1:length(Bac_terms))
   Bacgenesforterm2 <- paste(Bacgenesforterm2, collapse=',')
   print(paste("Term",Bac_GOterm,"transcripts:",Bacgenesforterm2))
 }
+#[1] "Term GO:0007264 transcripts: EKC20004,EKC21485,EKC24479,EKC26517,EKC28675,EKC39204"
+#[1] "Term GO:0006518 transcripts: EKC17829,EKC18290,EKC19096,EKC25781,EKC28353,EKC35385,EKC37643,EKC39112,EKC42018"
+
 
 #Find gene name for these genes, listed in OsHV1_data
 #need to remove "transcript:", add back new column
-Bac_data_edited1 <- removeWords(Bac_data$Ensembl_Genomes_Transcript, stopwords)
-Bac_data_edited2<- cbind(Bac_data, Bac_data_edited1)
+#Find gene name for these genes, listed in OsHV1_data
 #Lookup table with match(), return indices of match in second arg to first arg:
-Bac_sigGO_name_lookup <- read.csv("Bac_sigGO_transcriptIDs.csv", header=TRUE, sep=",")
-Bac_sigGO_gene_names_index <- Bac_data_edited2$Bac_data_edited1 %in% Bac_sigGO_name_lookup$Bac_data_edited1
-!is.na(match(Bac_data_edited2$Bac_data_edited1,Bac_sigGO_name_lookup$Bac_data_edited1))
-Bac_sigGO_gene_names <- Bac_data_edited2[Bac_sigGO_gene_names_index, ]
-write.csv(Bac_sigGO_gene_names, file="Bac_sigGO_gene_names.csv")
+Bac_sigGO_name_lookup <- read.csv("Bac_sigGO_transcriptIDs_NEW.csv", header=TRUE, sep=",")
+Bac_sigGO_gene_names_index <- Bac_GO_table$ID %in% Bac_sigGO_name_lookup$ID
+!is.na(match(Bac_GO_table$ID,Bac_GO_table$ID))
+Bac_sigGO_gene_names <- Bac_GO_table[Bac_sigGO_gene_names_index, ]
+write.csv(Bac_sigGO_gene_names, file="Bac_sigGO_gene_names_NEW.csv")
 
 ####COMPILED RESULTS TABLES ####
 Bac_numsignif_Res["Challenge"] <- "Bac"
 OsHV1_numsignif_Res["Challenge"] <- "OsHV1"
 COMBINED_TOPGO_RES_NUMSIGNIF <- rbind(Bac_numsignif_Res,OsHV1_numsignif_Res)
-write.csv(COMBINED_TOPGO_RES_NUMSIGNIF, file="COMBINED_TOPGO_NUMSIGNIF_RES.csv")
+write.csv(COMBINED_TOPGO_RES_NUMSIGNIF, file="COMBINED_TOPGO_NUMSIGNIF_RES_NEW.csv")
 Bac_topRes["Challenge"] <- "Bac"
 OsHV1_topRes["Challenge"] <- "OsHV1"
 COMBINED_TOPGO_RES_TOPSIGNIFICANTNODES <- rbind(Bac_topRes,OsHV1_topRes)
-write.csv(COMBINED_TOPGO_RES_TOPSIGNIFICANTNODES, file="COMBINED_TOPGO_RES_TOPSIGNIFICANTNODES.csv")
+write.csv(COMBINED_TOPGO_RES_TOPSIGNIFICANTNODES, file="COMBINED_TOPGO_RES_TOPSIGNIFICANTNODES_NEW.csv")
 Bac_sigGO_gene_names["Challenge"] <- "Bac"
 OsHV1_sigGO_gene_names["Challenge"] <- "OsHV1"
-#get rid of column with unmatching names
-Bac_sigGO_gene_names <- Bac_sigGO_gene_names[,c(1,2,3,4,5,6,8)]
-OsHV1_sigGO_gene_names <- OsHV1_sigGO_gene_names[,c(1,2,3,4,5,6,8)]
 COMBINED_TOPGO_RES_SIGGO_GENE_NAMES <- rbind(Bac_sigGO_gene_names,OsHV1_sigGO_gene_names)
 COMBINED_TOPGO_RES_SIGGO_GENE_NAMES <- COMBINED_TOPGO_RES_SIGGO_GENE_NAMES %>%
-  filter(Protein_names !="Uncharacterized protein")
-write.csv(COMBINED_TOPGO_RES_SIGGO_GENE_NAMES, file="COMBINED_TOPGO_RES_SIGGO_GENE_NAMES.csv")
+  filter(Protein.names !="Uncharacterized protein")
+write.csv(COMBINED_TOPGO_RES_SIGGO_GENE_NAMES, file="COMBINED_TOPGO_RES_SIGGO_GENE_NAMES_NEW.csv")
 
 #References: Gene set enrichment analysis with topGO
 #Adrian Alexa, Jorg Rahnenfuhrer, April 24, 2017, http://www.mpi-sb.mpg.de/alexa
