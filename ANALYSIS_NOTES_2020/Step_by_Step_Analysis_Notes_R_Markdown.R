3bb066871601aaa20e08c133842f2f8b3975e520
Erin Roberts <erin_roberts@my.uri.edu>
2020-01-30 16:53:43 -0500

    updated notes

--- ANALYSIS_NOTES_2020/Step_by_Step_Analysis_Notes_R_Markdown.R
+++ ANALYSIS_NOTES_2020/Step_by_Step_Analysis_Notes_R_Markdown.R
@@ -7,7 +7,7 @@ compare apoptosis gene expression between disease challenges.
 
 # 1/29/2020-1/30/2020 Data Preparation, Data Storage Plan
 
-  ## 1. Review Which SRAs to download and analyze from each full experiment
+  ## 1. Review Which SRAs to download and analyze from each full experiment:
 
       - C. virginica Probiotic = Download and analyze all available SRAs
       - C. virginica ROD = Download and analyze all available SRAs
@@ -17,30 +17,37 @@ compare apoptosis gene expression between disease challenges.
       - C. gigas Zhang et al., 2015 Vibrio = Download and analyze all available SRAs (only the
       transcriptomes that were challenged with individual strains of Vibrio, LPS, M. lut and PBS on NCBI)
       - C. gigas Rubio et al. 2019 Vibrio = Download and analyze all available SRAs
-      - C. gigas de Lorgeril et al., 2017 = Download and analyze only the samples that
-      were injected with OsHV-1?
+      - C. gigas de Lorgeril et al., 2017 = Download and analyze only the samples from families 11 and
+      21 that were infected with OsHV-1 during a "natural" infection and sequenced. These were all sequenced
+      paired end on hiseq 500. 42 total transcriptomes
 
   ## 2. Create New folders on bluewaves cluster where data will be housed.
 
-
-  ## 3.  Merge technical replicates from Proestou et al. 2015 transcriptomes
-
-  ## 4. C
+      - Data will be housed in the following folders
 
 
-  ## 5. Make an individual File for each experiment with the SRA's for each (not for Dermo transcriptomes)
 
-        $ cd /Users/erinroberts/Documents/PhD_Research/Chapter_1_Apoptosis Paper/Chapter1_Apoptosis_Transcriptome_Analyses_2019/DATA ANALYSIS/apoptosis_data_pipeline/Transcriptome_Bluewaves_SRA_Data_Analysis_2020/Bio_projects_Sample_Metadata
-        $ ls *SRA_ID.txt
-        $ C_gig_He_2015_OsHV1_SRA_ID.txt
-          C_vir_Probiotic_SRA_ID.txt
-          C_gig_Rubio_Vibrio_SRA_ID.txt
-          C_vir_ROD_SRA_ID.txt
-          C_gig_Zhang_Vibrio_SRA_ID.txt
-
-  ## Add metadata for all the samples to "Organized_SRA_info.xlsx" spreadsheet for reference later.
+  ## 3.  Merge technical replicates from Proestou et al. 2015 transcriptomes. Do this by concatenating
+  all technical rep 1 and 2 forward reads and technical rep 1 and 2 reverse reads.
 
 
-  ## Create Full_SRA_PE_list.txt and Full_SRA_SE_list.txt that has full SRA list with each species
+
+  ## 4. Make an individual text file for each experiment with the SRA's for each (not for Dermo transcriptomes),
+  and Create Full_SRA_PE_list.txt and Full_SRA_SE_list.txt that has full SRA list (with species combined)
+
+  $ pwd
+  /Users/erinroberts/Documents/PhD_Research/Chapter_1_Apoptosis Paper/Chapter1_Apoptosis_Transcriptome_Analyses_2019/DATA ANALYSIS/apoptosis_data_pipeline/Transcriptome_Bluewaves_Cluster_Analysis_2020/Bio_projects_Sample_Metadata        $ ls *SRA_ID.txt
+
+  $ ls *.txt
+      C_gig_He_2015_OsHV1_SRA_ID.txt		C_gig_deLorgeril_OsHV1_SRA_ID.txt	Full_SRA_PE_list.txt
+      C_gig_Rubio_Vibrio_SRA_ID.txt		C_vir_Probiotic_SRA_ID.txt		Full_SRA_SE_list.txt
+      C_gig_Zhang_Vibrio_SRA_ID.txt		C_vir_ROD_SRA_ID.txt
+
+
+  ## 6. Add metadata for all the samples to "Organized_SRA_info.xlsx" spreadsheet for reference later.
+
+        - Complete
+
+
 
 # 1/30/2020 Data Download from SRA database (except for Dermo transcriptomes from Dina)
