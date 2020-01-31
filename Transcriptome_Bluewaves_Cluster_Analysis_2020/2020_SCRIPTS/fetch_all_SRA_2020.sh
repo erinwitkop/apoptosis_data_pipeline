383d32a0e748bb92371d66b0184c984e06007d0d
Erin Roberts <erin_roberts@my.uri.edu>
2020-01-31 11:39:12 -0500

    edited path to download

--- Transcriptome_Bluewaves_Cluster_Analysis_2020/2020_SCRIPTS/fetch_all_SRA_2020.sh
+++ Transcriptome_Bluewaves_Cluster_Analysis_2020/2020_SCRIPTS/fetch_all_SRA_2020.sh
@@ -2,8 +2,8 @@
 #SBATCH -t 1000:00:00
 #SBATCH --nodes=1 --ntasks-per-node=1
 #SBATCH --export=NONE
-#SBATCH	-o fetch_SRA_output_1_31_2020
-#SBATCH	-e fetch_SRA_error_1_31_2020
+#SBATCH	-o /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/2020_Scripts/Script_out_error_files/fetch_SRA_output_1_31_2020
+#SBATCH	-e /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/2020_Scripts/Script_out_error_files/fetch_SRA_error_1_31_2020
 #SBATCH	--mail-user=erin_roberts@my.uri.edu
 
 echo "START $(date)"
@@ -13,8 +13,8 @@ module load SRA-Toolkit/2.9.0-centos_linux64
 
 # Create variable for each path to raw data folder for each species
 
-C=/data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/C_Vir_subset/2020_Raw_Transcriptome_Data/
-G=/data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/C_gig_Bac_Viral_subset/2020_Raw_Transcriptome_Data/
+C=/data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/C_Vir_subset/2020_Raw_Transcriptome_Data
+G=/data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_files/C_gig_Bac_Viral_subset/2020_Raw_Transcriptome_Data
 
 ####### Load single end read files #######
 
@@ -23,7 +23,7 @@ for f in $G/C_gig_He_OsHV1_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --readids $LINE
+    fastq-dump -O $G/C_gig_He_OsHV1_SRA/ --readids $LINE
   done < $f
 done
 
@@ -34,7 +34,7 @@ for f in $G/C_gig_Zhang_Vibrio_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --readids $LINE
+    fastq-dump -O $G/C_gig_Zhang_Vibrio_SRA/ --readids $LINE
   done < $f
 done
 
@@ -45,7 +45,7 @@ for f in $C/C_vir_ROD_SRA/C_vir_ROD_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --readids $LINE
+    fastq-dump -O $C/C_vir_ROD_SRA/ --readids $LINE
   done < $f
 done
 
@@ -59,7 +59,7 @@ for f in $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_deLorgeril_OsHV1_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --split-files --readids $LINE
+    fastq-dump -O $G/C_gig_deLorgeril_OsHV1_SRA/ --split-files --readids $LINE
   done < $f
 done
 
@@ -70,7 +70,7 @@ for f in $G/C_gig_Rubio_Vibrio_SRA/C_gig_Rubio_Vibrio_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --split-files --readids $LINE
+    fastq-dump -O $G/C_gig_Rubio_Vibrio_SRA/ --split-files --readids $LINE
   done < $f
 done
 
@@ -81,7 +81,7 @@ for f in $C/C_vir_Probiotic_SRA/C_vir_Probiotic_SRA_ID.txt
 do
   prefetch --option-file $f
   while read -r LINE; do
-    fastq-dump -O $F --split-files --readids $LINE
+    fastq-dump -O $C/C_vir_Probiotic_SRA/ --split-files --readids $LINE
   done < $f
 done
 
