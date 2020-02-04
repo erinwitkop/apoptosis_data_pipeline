0a7c73d5f5c583ff475aae14af68f6fcc9cf903c
Erin Roberts <erin_roberts@my.uri.edu>
2020-02-04 16:43:01 -0500

    final script

--- Transcriptome_Bluewaves_Cluster_Analysis_2020/2020_SCRIPTS/fetch_all_SRA_2020.sh
+++ Transcriptome_Bluewaves_Cluster_Analysis_2020/2020_SCRIPTS/fetch_all_SRA_2020.sh
@@ -19,59 +19,59 @@ G=/data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/pipeline_file
 ####### Load single end read files #######
 
 # Load C_gig_He_2015_OsHV1_SRA_ID
-#for f in $G/C_gig_He_OsHV1_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt
-#do
-#  prefetch --option-file $f
-#  while read -r LINE; do
-#    fastq-dump -O $G/C_gig_He_OsHV1_SRA/ --readids --gzip $LINE
-#  done < $f
-#done
-#echo "C_gig_He_2015_OsHV1_SRA_ID download done $(date)"
+for f in $G/C_gig_He_OsHV1_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt
+do
+  prefetch --option-file $f
+  while read -r LINE; do
+    fastq-dump -O $G/C_gig_He_OsHV1_SRA/ --readids --gzip $LINE
+  done < $f
+done
+echo "C_gig_He_2015_OsHV1_SRA_ID download done $(date)"
 
-#vdb-validate --option-file $G/C_gig_He_OsHV1_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt &>  $G/C_gig_He_OsHV1_SRA/OsHV1_He_sra_checksum.txt
+vdb-validate --option-file $G/C_gig_He_OsHV1_SRA/C_gig_He_2015_OsHV1_SRA_ID.txt &>  $G/C_gig_He_OsHV1_SRA/OsHV1_He_sra_checksum.txt
 
-#echo "C_gig_He_2015_OsHV1_SRA_ID validate DONE $(date)"
+echo "C_gig_He_2015_OsHV1_SRA_ID validate DONE $(date)"
 
 ## Load C_gig_Zhang_Vibrio_SRA_ID
-#for f in $G/C_gig_Zhang_Vibrio_SRA/C_gig_Zhang_Vibrio_SRA_ID.txt
-#do
-#  prefetch --option-file $f
-#  while read -r LINE; do
-#    fastq-dump -O $G/C_gig_Zhang_Vibrio_SRA/ --readids --gzip $LINE
-#  done < $f
-#done
+for f in $G/C_gig_Zhang_Vibrio_SRA/C_gig_Zhang_Vibrio_SRA_ID.txt
+do
+  prefetch --option-file $f
+  while read -r LINE; do
+    fastq-dump -O $G/C_gig_Zhang_Vibrio_SRA/ --readids --gzip $LINE
+  done < $f
+done
 
-#vdb-validate --option-file $G/C_gig_Zhang_Vibrio_SRA/C_gig_Zhang_Vibrio_SRA_ID.txt &>  $G/C_gig_Zhang_Vibrio_SRA/Zhang_Vibrio_sra_checksum.txt
+vdb-validate --option-file $G/C_gig_Zhang_Vibrio_SRA/C_gig_Zhang_Vibrio_SRA_ID.txt &>  $G/C_gig_Zhang_Vibrio_SRA/Zhang_Vibrio_sra_checksum.txt
 
-#echo "C_gig_Zhang_Vibrio_SRA_ID DONE $(date)"
+echo "C_gig_Zhang_Vibrio_SRA_ID DONE $(date)"
 
 ## Load C_vir_ROD_SRA_ID
-#for f in $C/C_vir_ROD_SRA/C_vir_ROD_SRA_ID.txt
-#do
-#  prefetch --option-file $f
-#  while read -r LINE; do
-#    fastq-dump -O $C/C_vir_ROD_SRA/ --readids --gzip $LINE
-#  done < $f
-#done
+for f in $C/C_vir_ROD_SRA/C_vir_ROD_SRA_ID.txt
+do
+  prefetch --option-file $f
+  while read -r LINE; do
+    fastq-dump -O $C/C_vir_ROD_SRA/ --readids --gzip $LINE
+  done < $f
+done
 
-# vdb-validate --option-file $C/C_vir_ROD_SRA/C_vir_ROD_SRA_ID.txt &>  $C/C_vir_ROD_SRA/C_vir_ROD_sra_checksum.txt
+ vdb-validate --option-file $C/C_vir_ROD_SRA/C_vir_ROD_SRA_ID.txt &>  $C/C_vir_ROD_SRA/C_vir_ROD_sra_checksum.txt
 
-#echo "C_vir_ROD_SRA_ID DONE $(date)"
+echo "C_vir_ROD_SRA_ID DONE $(date)"
 
 ###### Load paired end read files #####
 
 ## Load C_gig_deLorgeril_OsHV1
-#for f in $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_deLorgeril_OsHV1_SRA_ID.txt
-#do
-#  prefetch --option-file $f
-#  while read -r LINE; do
-#    fastq-dump -O $G/C_gig_deLorgeril_OsHV1_SRA/ --split-files --readids --gzip $LINE
-#  done < $f
-#done
+for f in $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_deLorgeril_OsHV1_SRA_ID.txt
+do
+  prefetch --option-file $f
+  while read -r LINE; do
+    fastq-dump -O $G/C_gig_deLorgeril_OsHV1_SRA/ --split-files --readids --gzip $LINE
+  done < $f
+done
 
-# vdb-validate --option-file $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_deLorgeril_OsHV1_SRA_ID.txt &>  $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_lorgeril_OsHV1_sra_checksum.txt
+ vdb-validate --option-file $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_deLorgeril_OsHV1_SRA_ID.txt &>  $G/C_gig_deLorgeril_OsHV1_SRA/C_gig_lorgeril_OsHV1_sra_checksum.txt
 
-#echo "C_gig_deLorgeril_OsHV1 DONE $(date)"
+echo "C_gig_deLorgeril_OsHV1 DONE $(date)"
 
 ## Load C_gig_Rubio_Vibrio_SRA_ID
 for f in $G/C_gig_Rubio_Vibrio_SRA/C_gig_Rubio_Vibrio_SRA_ID.txt
@@ -87,16 +87,16 @@ done
 echo "C_gig_Rubio_Vibrio_SRA DONE $(date)"
 
 ## Load C_vir_Probiotic_SRA_ID
-#for f in $C/C_vir_Probiotic_SRA/C_vir_Probiotic_SRA_ID.txt
-#do
-#  prefetch --option-file $f
-#  while read -r LINE; do
-#    fastq-dump -O $C/C_vir_Probiotic_SRA/ --split-files --readids --gzip $LINE
-#  done < $f
-#done
+for f in $C/C_vir_Probiotic_SRA/C_vir_Probiotic_SRA_ID.txt
+do
+  prefetch --option-file $f
+  while read -r LINE; do
+    fastq-dump -O $C/C_vir_Probiotic_SRA/ --split-files --readids --gzip $LINE
+  done < $f
+done
 
-# vdb-validate --option-file $C/C_vir_Probiotic_SRA/C_vir_Probiotic_SRA_ID.txt &>  $C/C_vir_Probiotic_SRA/C_vir_Probiotic_sra_checksum.txt
+ vdb-validate --option-file $C/C_vir_Probiotic_SRA/C_vir_Probiotic_SRA_ID.txt &>  $C/C_vir_Probiotic_SRA/C_vir_Probiotic_sra_checksum.txt
 
-#echo "C_vir_Probiotic_SRA_ID DONE $(date)"
+echo "C_vir_Probiotic_SRA_ID DONE $(date)"
 
 echo "STOP $(date)"
