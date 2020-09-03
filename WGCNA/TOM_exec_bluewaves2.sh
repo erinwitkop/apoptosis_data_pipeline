0fb5403537a55fced6efd3375c2fa103852157db
Erin Roberts <erin_roberts@my.uri.edu>
2020-09-03 09:26:09 -0500

    made node use exclusive and increased memory for job to 250GB

---
+++ WGCNA/TOM_exec_bluewaves2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -t 100:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --mem=250GB
+#SBATCH --export=NONE
+#SBATCH -o /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/Script_out_error_files/module_export_9_2_2020_out
+#SBATCH -e /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/Script_out_error_files/module_export_9_2_2020_error
+
+echo "START $(date)"
+
+# Load R version 3.6.0 (3.6.1 doesn't have any relevant bug fixes)
+module load R/3.6.0-intel-2019a
+
+Rscript /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/TOMsim_WGCNA_DESeq_cluster.R
+
+echo "DONE $(date)"
