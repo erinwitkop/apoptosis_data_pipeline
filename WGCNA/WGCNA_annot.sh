22a1d52c9017dfe446e258d7f3fa2d355599cc0f
Erin Roberts <erin_roberts@my.uri.edu>
2020-10-22 10:12:59 -0400

    adding script use to execute R script in bluewaves

---
+++ WGCNA/WGCNA_annot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -t 100:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --export=NONE
+#SBATCH -o /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/Script_out_error_files/module_annot_9_21_2020_out
+#SBATCH -e /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/Script_out_error_files/module_annot_9_21_2020_error
+
+echo "START $(date)"
+
+# Load R version 3.6.0 (3.6.1 doesn't have any relevant bug fixes)
+module load R/3.6.0-intel-2019a
+
+Rscript /data3/marine_diseases_lab/erin/2017_2020_Transcriptome_Analysis/WGCNA/SCRIPTS/WGCNA_annotate_apop.R
+
+echo "DONE $(date)"
